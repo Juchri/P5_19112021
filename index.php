31e910e5d734b6affb9059b3261597951ae929c4
Juchri <juliette.chri@gmail.com>
2021-11-27 10:16:59 +0100

    connexion to database

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
 <?php
-
-mysql_connect(
-    string $server = ini_get("mysql.default_host"),
-    string $username = ini_get("mysql.default_user"),
-    string $password = ini_get("mysql.default_password"),
-    bool $new_link = false,
-    int $client_flags = 0
-): resource|false
-
-
-$link = mysql_connect("localhost", "mysql_user", "mysql_password")
-    or die("Impossible de se connecter : " . mysql_error());
-echo 'Connexion réussie';
-mysql_close($link);
-
-
+        require ('config_db.php');
 ?>
